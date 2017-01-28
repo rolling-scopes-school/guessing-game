96f2c1e1a3e7821bafa0f4ad851db52f6df36183
savlevich <savlevich.alexander@gmail.com>
2017-01-28 21:51:46 +0300

    tests completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+ 		this.min = null;
+ 		this.max = null;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	return Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-
+    	this.max = this.guess();
     }
 
     greater() {
-
+    	this.min = this.guess();
     }
 }
 
