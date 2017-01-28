ef1f9525bd8cc2a64f874f1fc9bbdd1c48243a38
YauheniyaAniska <anisko.noun@gmail.com>
2017-01-28 20:24:26 +0300

    guessing-game: end

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,13 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.mid = 0;
+    }
 
-    setRange(min, max) {}
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
 
-    guess() {}
+    guess() {
+        this.mid = Math.round((this.min + this.max)/2);
+        return this.mid;
+    }
 
-    lower() {}
+    lower() {
+        this.max = this.mid ;
+    }
 
-    greater() {}
+    greater() {
+        this.min = this.mid ;
+    }
 }
 
 module.exports = GuessingGame;
