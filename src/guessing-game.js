055aaee3a6796168b07df53950d2b7559e2e6b3b
egorhormash <marmelad@MacBook-Pro-Egor.local>
2017-01-30 15:19:46 +0300

    guessing-game: passed all tests

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.minValue = 0;
+    	this.maxValue = 0;
+    	this.midValue = 0;
+    }
 
     setRange(min, max) {
-
+    	this.minValue = min;
+    	this.maxValue = max;
     }
 
     guess() {
-
+    	this.midValue = Math.round((this.minValue + this.maxValue) / 2);
+    	return this.midValue ;
     }
 
     lower() {
-
+    	this.maxValue = this.guess();
     }
 
     greater() {
-
+    	this.minValue = this.guess();
     }
 }
 
