724313daeca8ac0f393d7575a7160bc816970f8f
Yana <yanalapitskay@gmail.com>
2017-09-14 18:10:12 +0100

    added solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,34 @@
+var mn,mx,average,isLower,isGreater,result;
+
 class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+    	mn=min;
+    	mx=max;
+    	isLower=false;
+    	isGreater=false;
     }
 
     guess() {
-
+    	if(isLower){
+    		mx=average;
+    	}
+    	else if(isGreater){
+    		mn=average;
+    	}
+    	average=Math.ceil(mn + ((mx - mn)/2));
+    	return average;
     }
 
     lower() {
-
+    	isLower=true;
+    	isGreater=false
     }
 
     greater() {
-
+    	isGreater=true;
+    	isLower=false;
     }
 }
 
