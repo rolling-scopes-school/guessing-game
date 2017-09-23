f7810563ea7c6266e2dc9151307cdbcc7324bd3d
Alexander <guts-817@rambler.ru>
2017-09-23 18:02:36 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,27 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+         this.min=min;
+         this.max=max;
+      
+         this.a=[];
+         for (var i=0; i<this.max-this.min+1; i++) {
+             this.a[i]=this.min+i;
+          } 
     }
 
     guess() {
-
+         this.mid=(this.min+this.max)/2;
+         if (this.mid>parseInt(this.mid)) this.mid=this.mid+0.5; 
+         return this.a[this.mid];
     }
 
     lower() {
-
+         this.max=this.mid;
     }
 
     greater() {
-
+          this.min=this.mid;
     }
 }
 
