47a323f1d2895471c212578b0509867cf9fea0a2
Siarhei-Chaptsau <s.chaptsau@gmail.com>
2017-09-26 00:36:23 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.minimal = 0;
+      this.maximum = 0;
+      this.mid = 0;
+    }
 
     setRange(min, max) {
-
+      this.minimal = min;
+      this.maximum = max;
     }
 
     guess() {
-
+      this.mid = Math.ceil((this.minimal + this.maximum) / 2);
+      return this.mid;
     }
 
     lower() {
-
+      this.maximum = this.mid;
     }
 
     greater() {
-
+      this.minimal = this.mid;
     }
 }
 
