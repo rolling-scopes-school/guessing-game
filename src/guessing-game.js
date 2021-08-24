04cd3112c0e2256f12a796c1d06dcf055b3ea910
coolmanben <76854955+coolmanben@users.noreply.github.com>
2021-08-25 01:46:41 +0300

    init: start task
    init: start task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.left = null;
+        this.rigth = null;
+        this.middle = null;
+
+    }
 
     setRange(min, max) {
-
+        this.left  = min;
+        this.rigth = max;
     }
 
     guess() {
-
+        let middle =  Math.floor( ( this.left + this.right ) / 2 ) + this.left;
+        this.middle  = middle
+        return middle
     }
 
     lower() {
-
+        this.rigth = this.middle;
     }
 
     greater() {
-
+        this.left =  this.middle;
     }
 }
 
