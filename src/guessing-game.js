13aacda324b19eea81abbb1c92c8a1a171046bc7
AmdreiMash <100795104+AmdreiMash@users.noreply.github.com>
2022-08-13 16:47:42 +0300

    complate the tsak

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,44 @@
 class GuessingGame {
-    constructor() {}
+	constructor() {
+		this.max = 0
+		this.min = 0
+		this.prew = 0
+	}
+	setRange(min, max) {
+		this.max = max;
+		this.min = min;
+	}
 
-    setRange(min, max) {
+	guess() {
+		return Math.round(this.min + ((this.max - this.min) / 2))
+	}
 
-    }
+	lower() {
+		this.max = this.guess();
+	}
 
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	greater() {
+		this.min = this.guess();
+	}
 }
 
 module.exports = GuessingGame;
+
+const game = new GuessingGame();
+game.setRange(0, 3445)
+
+game.lower();
+game.lower();
+game.lower();
+game.greater();
+game.lower();
+game.greater();
+game.lower();
+game.lower();
+game.greater();
+game.lower();
+
+console.log(game.guess())
+console.log(game.max)
+console.log(game.min)
+
