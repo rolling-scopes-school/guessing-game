403bedb6b50eca5b08b3de6db0cae6f2eab5683a
Ihar Skavysh <igogoblin@gmail.com>
2022-08-15 07:16:19 +0300

    feat: add the solution of the problem

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const GuessingGame = require('./src/guessing-game.js');
 
 window.game = new GuessingGame();
+
+class GuessingGame {
+constructor(minValue, maxValue){
+    this.minValue;
+    this.maxValue;
+}
+setRange(min,max){
+    this.minValue=min;
+    this.maxValue = max;
+}
+guess(){
+    return Math.round((this.maxValue - this.minValue)/2);
+}
+lower() {
+    this.maxValue = Math.round((this.maxValue - this.minValue)/2);
+}
+greater(){
+    this.minValue = Math.round((this.maxValue-this.minValue)/2);
+}
+}
+module.exports = GuessingGame;
--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minValue;
+        this.maxValue;
+    }
 
     setRange(min, max) {
-
+        this.minVal = min;
+        this.maxVal = max;
     }
 
     guess() {
-
+        return Math.round((this.maxVal + this.minVal)/2);
     }
 
     lower() {
-
+        this.maxVal = Math.round((this.maxVal + this.minVal)/2);
     }
 
     greater() {
-
+        this.minVal = Math.round((this.maxVal+this.minVal)/2);
     }
 }
 
