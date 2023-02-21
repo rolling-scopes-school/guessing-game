8569343e5fe68859407122627668872256382c9b
LizavetaNik <39527492+LizavetaNik@users.noreply.github.com>
2023-02-21 16:06:14 +0200

    init: add decision2

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+       this.min = min;
+       this.max = max;
     }
 
     guess() {
-
+        return Math.ceil( ( this.min + this.max ) / 2 );
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
