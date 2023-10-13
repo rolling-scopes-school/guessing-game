2df57b8d2e6ddcc1fbd2f6698cf856af22675d61
Vladzislav Khurs <130601125+VladKhurs@users.noreply.github.com>
2023-10-13 21:49:32 +0200

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        return Math.floor((this.max + this.min) / 2)
     }
 
     lower() {
-
+        this.max = Math.floor((this.max + this.min) / 2) + 1
     }
 
     greater() {
-
+        this.min = Math.floor((this.max + this.min) / 2)
     }
 }
 
